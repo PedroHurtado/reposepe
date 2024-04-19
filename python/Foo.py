d8b7384bafd494cf0235423839bc2ad6b1e7f2c8
Pedro Hurtado <pedrohurt@gmail.com>
2024-04-19 12:59:37 +0200

    redefine hash

--- python/Foo.py
+++ python/Foo.py
@@ -5,10 +5,15 @@ class Foo:
         implementar el codigo necesario para que las clases con id igual
         no se dupliquen en un set
         """
+    def __eq__(self, value: object) -> bool:
+        return self.id==value.id
+    def __hash__(self) -> int:
+        return hash(self.id)
 
 
-setter = set({Foo(5),Foo(5)})
+print(Foo(5)==Foo(5))
+setter = {Foo(5),Foo(5)}
 print(len(setter)) #2
 
-setter = set({1,1})
+setter = {1,1}
 print(len(setter)) #1

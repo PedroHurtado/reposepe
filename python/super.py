c864cc8aca420cc1961c4759fdb0de045bfbee2c
Pedro Hurtado <pedrohurt@gmail.com>
2024-04-23 10:39:05 +0200

    super

---
+++ python/super.py
@@ -0,0 +1,32 @@
+"""
+    crear una clase shape que reciba la x y la y 
+    crear una clase rectangulo que hereda de la clase shape
+    crear los correspondientes getter y setter para x e y
+    https://docs.python.org/3/library/abc.html
+"""
+
+class Shape:
+    def __init__(self,x,y) -> None:
+        self.x =x
+        self.y =y
+    @property
+    def x(self):
+        return self._x
+    @x.setter
+    def x(self,x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+    @y.setter
+    def y(self,y):
+        self._y= y
+
+class Square(Shape):
+    def __init__(self, x, y) -> None:
+        super().__init__(x, y)
+
+square = Square(10,10)
+print(square.x)
+print(square.y)

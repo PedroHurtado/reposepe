97b2c1f0b16fbbb3e071ecc22600ca6f2a2aa11f
Pedro Hurtado <pedrohurt@gmail.com>
2024-04-23 09:47:42 +0200

    classmethod

--- python/method.py
+++ python/method.py
@@ -1,17 +1,27 @@
 class Foo:
-    def instance(self):
-        pass
-    @classmethod
-    def cl(cls):
-        pass
-    @staticmethod
-    def static():
-        pass
+	name=''
+	def __init__(self,name) -> None:
+		self.name = name
+	@classmethod
+	def cl(cls,name):
+		cls.name = name
+	@staticmethod
+	def static(age):		
+		return age<=18
+    
 
-foo = Foo()
-foo.cl()
-foo.static()
+foo = Foo("Pedro")
+print(foo.name) #Pedro
+foo.static(18)
+foo.cl("foo")  #''''
+
+foo1 = Foo("Pedro Hurtado")
+print(foo1.name) #Pedro Hurtado
+foo1.cl("foo1") #''''
+
+
 
 """
 crear la instancia de una clase con un metodo estatico y un class method
+https://www.geeksforgeeks.org/class-method-vs-static-method-python/
 """
